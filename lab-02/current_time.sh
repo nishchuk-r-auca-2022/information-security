5dab526a6128680b82f124b0afcd8acfe2ae9322
Rodion Nischuk <ramzesska57@gmail.com>
2025-01-30 10:30:47 +0600

    Task 2 bug fixes

--- lab-02/current_time.sh
+++ lab-02/current_time.sh
@@ -1,7 +1,18 @@
 end_time="18:00"
 current_time=$(date +"%H:%M")
 
-remaining=$(date -u -d "00:00 $(date -d "$end_time today" +%s) sec - $(date -d "$current_time today" +%s) sec" +"%H:%M")
+current_seconds=$(date -j -f "%H:%M" "$current_time" "+%s")
+end_seconds=$(date -j -f "%H:%M" "$end_time" "+%s")
 
-echo "Current time: $current_time"
-echo "Work day ends after $remaining"
+remaining_seconds=$(( end_seconds - current_seconds ))
+
+if [ $remaining_seconds -gt 0 ]; then
+    hours=$(( remaining_seconds / 3600 ))
+    minutes=$(( (remaining_seconds % 3600) / 60 ))
+    echo "Current time: $current_time"
+    echo "Work day ends after ${hours} hours and ${minutes} minutes"
+else
+    echo "Current time: $current_time"
+    echo "Work day is over."
+fi
+
