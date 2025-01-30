7937e610a6d94ce08f24dc7155de92ffa9b8d6ab
Rodion Nischuk <ramzesska57@gmail.com>
2025-01-30 10:38:42 +0600

    task 3

---
+++ lab-02/count_word.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <filename> <word>"
+    exit 1
+fi
+
+file=$1
+word=$2
+
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found!"
+    exit 1
+fi
+
+count=$(grep -o -w "$word" "$file" | wc -l | tr -d ' ')
+echo "The word '$word' appears $count times in '$file'."
+
